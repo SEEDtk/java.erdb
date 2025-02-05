4c91397c156b6a9cddaf7c18bf9f28ee136fe4b7
Bruce Parrello <bparrello@figresearch.com>
2025-02-05 04:56:16 -0600

    Test data fixes and configuration updates.

--- src/test/java/org/theseed/java/erdb/mysql/TestMysqlConnection.java
+++ src/test/java/org/theseed/java/erdb/mysql/TestMysqlConnection.java
@@ -3,15 +3,15 @@
  */
 package org.theseed.java.erdb.mysql;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.not;
-import static org.hamcrest.Matchers.nullValue;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-import org.junit.jupiter.api.Test;
-import org.theseed.java.erdb.CommonTesters;
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.not;
+//import static org.hamcrest.Matchers.nullValue;
+//
+//import java.io.IOException;
+//import java.sql.SQLException;
+//
+//import org.junit.jupiter.api.Test;
+//import org.theseed.java.erdb.CommonTesters;
 
 /**
  * To run this test you must have a MySQL database "rnaseq" on localhost with a user named "tester"
@@ -22,13 +22,13 @@ import org.theseed.java.erdb.CommonTesters;
  */
 public class TestMysqlConnection {
 
-    @Test
-    public void testMysql() throws SQLException, IOException {
-        // We connect to the database and run the stress test in TestLoading.
-        try (MysqlDbConnection db = new MysqlDbConnection("localhost/rnaseq", "user=tester&password=poobah")) {
-            assertThat(db, not(nullValue()));
-            db.clearTables();
-            CommonTesters.testLoading(db);
-        }
-    }
+//    @Test
+//    public void testMysql() throws SQLException, IOException {
+//        // We connect to the database and run the stress test in TestLoading.
+//        try (MysqlDbConnection db = new MysqlDbConnection("localhost/rnaseq", "user=tester&password=poobah")) {
+//            assertThat(db, not(nullValue()));
+//            db.clearTables();
+//            CommonTesters.testLoading(db);
+//        }
+//    }
 }
